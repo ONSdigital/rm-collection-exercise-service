9175b77e19cdf3036dfceaf679b407a5a9e7ad31
Sarah Radford <sarah.radford@ons.gov.uk>
2017-05-08 12:09:03 +0000

    CTPA-1251 Collection Exercise Seeddata

---
+++ collectionexercisesvc/src/main/resources/database/changes/release-10.37.0/BRES_CollectionExercise_seeddata.sql
@@ -0,0 +1,9 @@
+SET schema 'collectionexercise';
+
+INSERT INTO collectionexercise(exerciseid,scheduledstartdatetime,scheduledexecutiondatetime,scheduledreturndatetime,scheduledenddatetime,scheduledsurveydate,state)
+VALUES (1,'2017-09-01','2017-08-24','2017-10-07','2018-03-31','2017-09-09','INIT');
+
+INSERT INTO survey(surveyid,name) VALUES(1,'BRES_2016'); 
+
+INSERT INTO casetypedefault (casetypedefaultid,surveyid,sampleunittype,actionplanid) VALUES (1,1,'B',1);
+INSERT INTO casetypedefault (casetypedefaultid,surveyid,sampleunittype,actionplanid) VALUES (2,1,'BI',2);
