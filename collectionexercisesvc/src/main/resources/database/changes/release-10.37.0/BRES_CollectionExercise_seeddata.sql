e3fb0e844c58635dcdd46c8c06084150e9076ee5
Sarah Radford <sarahradford@ip-172-29-102-97.eu-west-1.compute.internal>
2017-05-22 12:46:18 +0100

    CTPA-1251 Foundation schema and seed data

--- collectionexercisesvc/src/main/resources/database/changes/release-10.37.0/BRES_CollectionExercise_seeddata.sql
+++ collectionexercisesvc/src/main/resources/database/changes/release-10.37.0/BRES_CollectionExercise_seeddata.sql
@@ -1,13 +1,11 @@
-SET schema 'collectionexercise';
+﻿SET schema 'collectionexercise';
 
-INSERT INTO survey(surveyid,surveyref) VALUES('cb0711c3-0ac8-41d3-ae0e-567e5ea1ef87','BRES_2016');
-INSERT INTO survey(surveyid,surveyref) VALUES('cb0711c3-0ac8-41d3-ae0e-567e5ea1ef88','BRES_2017');
+INSERT INTO survey(id, surveyid, surveyref) VALUES('99f6cd6d-880c-4b36-b157-aeda409ec441',1,'BRES_2016'); 
 
-INSERT INTO collectionexercise(surveyid,exerciseid,scheduledstartdatetime,scheduledexecutiondatetime,scheduledreturndatetime,scheduledenddatetime,scheduledsurveydate,state)
-VALUES ('cb0711c3-0ac8-41d3-ae0e-567e5ea1ef87','c6467711-21eb-4e78-804c-1db8392f93fb','2017-09-01','2017-08-24','2017-10-07','2018-03-31','2017-09-09','INIT'),
-('cb0711c3-0ac8-41d3-ae0e-567e5ea1ef87','c6467711-21eb-4e78-804c-1db8392f93fc','2017-09-01','2017-08-24','2017-10-07','2018-03-31','2017-09-09','INIT'),
-('cb0711c3-0ac8-41d3-ae0e-567e5ea1ef87','c6467711-21eb-4e78-804c-1db8392f93fd','2017-09-01','2017-08-24','2017-10-07','2018-03-31','2017-09-09','INIT'),
-('cb0711c3-0ac8-41d3-ae0e-567e5ea1ef88','c6467711-21eb-4e78-804c-1db8392f93fe','2017-09-01','2017-08-24','2017-10-07','2018-03-31','2017-09-09','INIT');
+INSERT INTO collectionexercise(id,surveyid,exerciseid,name,scheduledstartdatetime,scheduledexecutiondatetime,scheduledreturndatetime,scheduledenddatetime,periodstartdatetime,state)
+VALUES ('14fb3e68-4dca-46db-bf49-04b84e07e77c',1,1,'201601','2017-09-01','2017-08-24','2017-10-07','2018-03-31','2017-09-09','INIT');
 
-INSERT INTO casetypedefault (casetypedefaultid,surveyid,sampleunittype,actionplanid) VALUES (1,'cb0711c3-0ac8-41d3-ae0e-567e5ea1ef87','B',1);
-INSERT INTO casetypedefault (casetypedefaultid,surveyid,sampleunittype,actionplanid) VALUES (2,'cb0711c3-0ac8-41d3-ae0e-567e5ea1ef88','BI',2);
+INSERT INTO casetypedefault (casetypedefaultid,surveyid,sampleunittype,actionplanid) VALUES (1,1,'B',1);
+INSERT INTO casetypedefault (casetypedefaultid,surveyid,sampleunittype,actionplanid) VALUES (2,1,'BI',2);
+
+
