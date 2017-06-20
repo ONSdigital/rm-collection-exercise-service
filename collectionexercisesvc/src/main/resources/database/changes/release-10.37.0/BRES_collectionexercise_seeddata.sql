33de533fb6ed1472a3eadbf8861fadc9fd2e25c7
Sarah Radford <sarah.radford@ons.gov.uk>
2017-06-20 13:16:32 +0000

    Collection Exercise seed data

--- collectionexercisesvc/src/main/resources/database/changes/release-10.37.0/BRES_collectionexercise_seeddata.sql
+++ collectionexercisesvc/src/main/resources/database/changes/release-10.37.0/BRES_collectionexercise_seeddata.sql
@@ -10,10 +10,10 @@ INSERT INTO   state(statePK) VALUES('FAILEDVALIDATION');
 INSERT INTO   sampleunittype(sampleunittypePK) VALUES('B');
 INSERT INTO   sampleunittype(sampleunittypePK) VALUES('BI');
 
-INSERT INTO survey(id, surveyPK, surveyref) VALUES('99f6cd6d-880c-4b36-b157-aeda409ec441',1,'BRES_2016'); 
+INSERT INTO survey(id, surveyPK, surveyref) VALUES('99f6cd6d-880c-4b36-b157-aeda409ec441',1,'221'); 
 
 INSERT INTO collectionexercise(id,surveyFK,exercisePK,name,scheduledstartdatetime,scheduledexecutiondatetime,scheduledreturndatetime,scheduledenddatetime,periodstartdatetime,periodenddatetime,stateFK)
-VALUES ('14fb3e68-4dca-46db-bf49-04b84e07e77c',1,1,'201601','2017-08-30',NULL,'2017-10-06',NULL,'2017-09-08','2017-09-08','INIT');
+VALUES ('14fb3e68-4dca-46db-bf49-04b84e07e77c',1,1,'BRES_2016','2017-08-30',NULL,'2017-10-06',NULL,'2017-09-08','2017-09-08','INIT');
 
 INSERT INTO casetypedefault (casetypedefaultPK,surveyFK,sampleunittypeFK,actionplanId) VALUES (1,1,'B' ,'e71002ac-3575-47eb-b87f-cd9db92bf9a7');
 INSERT INTO casetypedefault (casetypedefaultPK,surveyFK,sampleunittypeFK,actionplanId) VALUES (2,1,'BI','0009e978-0932-463b-a2a1-b45cb3ffcb2a');
