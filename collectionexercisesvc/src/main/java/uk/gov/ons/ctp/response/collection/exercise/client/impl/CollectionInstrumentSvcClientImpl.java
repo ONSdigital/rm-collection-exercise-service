972deda9ace3b0e72010537212398702098a00c5
Edward Stevens <edward.stevens@ons.gov.uk>
2017-06-28 12:34:38 +0100

    CTPA-1338 Updated CollectionInstrumentSvcClientImpl to use new getResourcesWithJsonParam method

--- collectionexercisesvc/src/main/java/uk/gov/ons/ctp/response/collection/exercise/client/impl/CollectionInstrumentSvcClientImpl.java
+++ collectionexercisesvc/src/main/java/uk/gov/ons/ctp/response/collection/exercise/client/impl/CollectionInstrumentSvcClientImpl.java
@@ -1,25 +1,17 @@
 package uk.gov.ons.ctp.response.collection.exercise.client.impl;
 
-import java.net.URLEncoder;
-import java.util.Arrays;
-import java.util.List;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import lombok.extern.slf4j.Slf4j;
 import uk.gov.ons.ctp.common.rest.RestClient;
 import uk.gov.ons.ctp.response.collection.exercise.client.CollectionInstrumentSvcClient;
 import uk.gov.ons.ctp.response.collection.exercise.config.AppConfig;
 import uk.gov.ons.ctp.response.collectionInstrument.representation.CollectionInstrumentDTO;
+
+import java.util.List;
 
 @Slf4j
 @Component
@@ -34,27 +26,15 @@ public class CollectionInstrumentSvcClientImpl implements CollectionInstrumentSv
 
   @Override
   public List<CollectionInstrumentDTO> requestCollectionInstruments(String searchString) {
-    
-    //temporarily not using RestClient from common due to collectionInstrument searchString requiring JSON
-    
-    MultiValueMap<String, String> queryParams = new HttpHeaders();
-    queryParams.add("searchString", URLEncoder.encode(searchString));
+
+    MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+    queryParams.add("searchString", "{searchStringValue}");
 
     String path = appConfig.getCollectionInstrumentSvc().getRequestCollectionInstruments();
-    UriComponents uriComponents = UriComponentsBuilder.newInstance()
-        .scheme(appConfig.getCollectionInstrumentSvc().getConnectionConfig().getScheme())
-        .host(appConfig.getCollectionInstrumentSvc().getConnectionConfig().getHost())
-        .port(appConfig.getCollectionInstrumentSvc().getConnectionConfig().getPort())
-        .path(path)
-        .queryParams(queryParams)
-        .build(true);
 
-    RestTemplate restTemplate = new RestTemplate();
+    List<CollectionInstrumentDTO> collectionInstrumentDTOList = collectionInstrumentSvcClientRestTemplate.getResourcesWithJsonParam(path, CollectionInstrumentDTO[].class, null, queryParams, searchString);
 
-    log.debug("Enter getResources for path {}", path);
-    ResponseEntity<CollectionInstrumentDTO[]> responseEntity = restTemplate.exchange(uriComponents.toUri(),
-        HttpMethod.GET, null, CollectionInstrumentDTO[].class);
-    return Arrays.asList(responseEntity.getBody());
+    return collectionInstrumentDTOList;
   }
 
 }
