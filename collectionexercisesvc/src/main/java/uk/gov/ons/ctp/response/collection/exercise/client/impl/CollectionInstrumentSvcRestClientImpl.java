e1355d5ba290485d9456f7211b3ba6258c923502
Martin Humphrey <martin.humphrey@ons.gov.uk>
2017-07-03 15:55:44 +0000

    CTPA-1338 Fix Rest Client to deal with query params containing braces

--- collectionexercisesvc/src/main/java/uk/gov/ons/ctp/response/collection/exercise/client/impl/CollectionInstrumentSvcRestClientImpl.java
+++ collectionexercisesvc/src/main/java/uk/gov/ons/ctp/response/collection/exercise/client/impl/CollectionInstrumentSvcRestClientImpl.java
@@ -1,40 +1,41 @@
 package uk.gov.ons.ctp.response.collection.exercise.client.impl;
 
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+
 import uk.gov.ons.ctp.common.rest.RestClient;
 import uk.gov.ons.ctp.response.collection.exercise.client.CollectionInstrumentSvcClient;
 import uk.gov.ons.ctp.response.collection.exercise.config.AppConfig;
 import uk.gov.ons.ctp.response.collectionInstrument.representation.CollectionInstrumentDTO;
 
-import java.util.List;
-
-@Slf4j
+/**
+ * HTTP RestClient implementation for calls to the Collection Instrument service.
+ *
+ */
 @Component
-public class CollectionInstrumentSvcClientImpl implements CollectionInstrumentSvcClient {
+public class CollectionInstrumentSvcRestClientImpl implements CollectionInstrumentSvcClient {
 
   @Autowired
   private AppConfig appConfig;
 
   @Autowired
   @Qualifier("collectionInstrumentSvc")
-  public RestClient collectionInstrumentSvcClientRestTemplate;
+  private RestClient collectionInstrumentSvcClientRestTemplate;
 
   @Override
   public List<CollectionInstrumentDTO> requestCollectionInstruments(String searchString) {
 
     MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
-    queryParams.add("searchString", "{searchStringValue}");
 
-    String path = appConfig.getCollectionInstrumentSvc().getRequestCollectionInstruments();
-
-    List<CollectionInstrumentDTO> collectionInstrumentDTOList = collectionInstrumentSvcClientRestTemplate.getResourcesWithJsonParam(path, CollectionInstrumentDTO[].class, null, queryParams, searchString);
-
-    return collectionInstrumentDTOList;
+    queryParams.add("searchString", searchString);
+    List<CollectionInstrumentDTO> instruments = collectionInstrumentSvcClientRestTemplate.getResources(
+        appConfig.getCollectionInstrumentSvc().getRequestCollectionInstruments(),
+        CollectionInstrumentDTO[].class, null, queryParams);
+    return instruments;
   }
-
 }
