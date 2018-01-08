66c4ae9a0189ce0c28b7bd1e317068eb492e8410
Matt Innes <matt@madeye.com>
2018-01-08 14:45:54 +0000

    Added sequence and fixed typo in column name

--- src/main/resources/database/changes/release-10.49.2/us004-event-table.sql
+++ src/main/resources/database/changes/release-10.49.2/us004-event-table.sql
@@ -1,3 +1,10 @@
+CREATE SEQUENCE collectionexercise.eventpkseq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 999999999999
+  START 1
+  CACHE 1;
+
 CREATE TABLE collectionexercise.event (
   id uuid UNIQUE,
   eventpk bigint PRIMARY KEY,
@@ -5,7 +12,7 @@ CREATE TABLE collectionexercise.event (
   tag VARCHAR(20),
   timestamp TIMESTAMP,
   created TIMESTAMP,
-  update TIMESTAMP,
+  updated TIMESTAMP,
   deleted BOOLEAN,
   UNIQUE(collexfk, tag)
 );
