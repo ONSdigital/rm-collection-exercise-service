7049aeae5a14456b99c37fee7b51e2d79d0f92d6
Ben Jefferies <benjefferies@echosoft.uk>
2018-02-15 13:14:03 +0000

    Update published CEs to LIVE or READY_FOR_LIVE

--- src/main/resources/database/changes/release-10.49.2/us042-deprecate-published.sql
+++ src/main/resources/database/changes/release-10.49.2/us042-deprecate-published.sql
@@ -1,3 +1,17 @@
-UPDATE collectionexercise.collectionexercise SET statefk = 'READY_FOR_LIVE' WHERE statefk = 'PUBLISHED';
+UPDATE collectionexercise.collectionexercise AS c
+ SET statefk = 'LIVE'
+ FROM collectionexercise.event AS e
+ WHERE c.exercisepk = e.collexfk
+ AND (tag = 'go_live' and timestamp <= now())
+ OR c.actualpublishdatetime <= now()
+ AND statefk = 'PUBLISHED';
+
+UPDATE collectionexercise.collectionexercise AS c
+ SET statefk = 'READY_FOR_LIVE'
+ FROM collectionexercise.event AS e
+ WHERE c.exercisepk = e.collexfk
+ AND (tag = 'go_live' and timestamp > now())
+ OR c.actualpublishdatetime > now()
+ AND statefk = 'PUBLISHED';
 
 DELETE FROM collectionexercise.collectionexercisestate WHERE statepk = 'PUBLISHED';
