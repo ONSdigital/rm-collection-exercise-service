55cbda5b2fae24fa9a08b7ab602c5cd0641914e4
madeye-matt <matt@madeye.com>
2018-06-11 09:01:19 +0100

    Revert "Added an integration test to test receiving sample upload finished messages (#63)" (#66)
    This reverts commit 3f442f0205297cd4040553ad4c8860f82f906ba4.

--- src/main/java/uk/gov/ons/ctp/response/collection/exercise/config/AppConfig.java
+++ src/main/java/uk/gov/ons/ctp/response/collection/exercise/config/AppConfig.java
@@ -5,7 +5,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.Data;
-import uk.gov.ons.ctp.common.message.rabbit.Rabbitmq;
 
 /**
  * The apps main holder for centralised configuration read from application.yml
@@ -25,6 +24,5 @@ public class AppConfig {
   private RedissonConfig redissonConfig;
   private ScheduleSettings schedules;
   private SwaggerSettings swaggerSettings;
-  private Rabbitmq rabbitmq;
 
 }
