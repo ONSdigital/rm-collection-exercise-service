2c0521eb3fcee5c502559e12aca8dc2b3c4d10a9
Andrew Millar <andrew.millar@qa.com>
2018-09-19 10:53:33 +0100

    Refactor surveyserviceclient and add tests for it

--- src/main/java/uk/gov/ons/ctp/response/collection/exercise/client/SurveySvcClient.java
+++ src/main/java/uk/gov/ons/ctp/response/collection/exercise/client/SurveySvcClient.java
@@ -1,14 +1,12 @@
 package uk.gov.ons.ctp.response.collection.exercise.client;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.godaddy.logging.Logger;
 import com.godaddy.logging.LoggerFactory;
-import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -39,18 +37,14 @@ public class SurveySvcClient {
   private final RestTemplate restTemplate;
   private final RestUtility restUtility;
 
-  private final ObjectMapper objectMapper;
-
   @Autowired
   public SurveySvcClient(
       AppConfig appConfig,
       RestTemplate restTemplate,
-      @Qualifier("surveyRestUtility") RestUtility restUtility,
-      @Qualifier("customObjectMapper") ObjectMapper objectMapper) {
+      @Qualifier("surveyRestUtility") RestUtility restUtility) {
     this.appConfig = appConfig;
     this.restTemplate = restTemplate;
     this.restUtility = restUtility;
-    this.objectMapper = objectMapper;
   }
 
   /**
@@ -58,38 +52,24 @@ public class SurveySvcClient {
    *
    * @param surveyId UUID for which to request classifiers.
    * @return List of SurveyClassifierDTO classifier selectors.
-   * @throws RestClientException something went wrong making http call.
    */
   @Retryable(
       value = {RestClientException.class},
       maxAttemptsExpression = "#{${retries.maxAttempts}}",
       backoff = @Backoff(delayExpression = "#{${retries.backoff}}"))
-  public List<SurveyClassifierDTO> requestClassifierTypeSelectors(final UUID surveyId)
-      throws RestClientException {
-
+  public List<SurveyClassifierDTO> requestClassifierTypeSelectors(final UUID surveyId) {
+    log.with("survey_id", surveyId).debug("Retrieving survey");
     UriComponents uriComponents =
         restUtility.createUriComponents(
             appConfig.getSurveySvc().getRequestClassifierTypesListPath(), null, surveyId);
-
-    HttpEntity<List<SurveyClassifierDTO>> httpEntity = restUtility.createHttpEntity(null);
-
-    log.with("survey_id", surveyId).debug("Retrieving survey");
-    ResponseEntity<String> responseEntity =
-        restTemplate.exchange(uriComponents.toUri(), HttpMethod.GET, httpEntity, String.class);
-
-    List<SurveyClassifierDTO> result = null;
-    if (responseEntity != null && responseEntity.getStatusCode().is2xxSuccessful()) {
-      String responseBody = responseEntity.getBody();
-      try {
-        result =
-            objectMapper.readValue(responseBody, new TypeReference<List<SurveyClassifierDTO>>() {});
-      } catch (IOException e) {
-        String msg = String.format("cause = %s - message = %s", e.getCause(), e.getMessage());
-        log.error(msg);
-      }
-    }
-
-    return result;
+    HttpEntity<List<SurveyClassifierDTO>> httpEntity = restUtility.createHttpEntityWithAuthHeader();
+    ResponseEntity<List<SurveyClassifierDTO>> responseEntity =
+        restTemplate.exchange(
+            uriComponents.toUri(),
+            HttpMethod.GET,
+            httpEntity,
+            new ParameterizedTypeReference<List<SurveyClassifierDTO>>() {});
+    return responseEntity.getBody();
   }
 
   /**
@@ -98,81 +78,40 @@ public class SurveySvcClient {
    * @param surveyId UUID for which to request classifiers.
    * @param classifierType UUID for classifier type.
    * @return SurveyClassifierTypeDTO details of selector type requested.
-   * @throws RestClientException something went wrong making http call.
    */
   @Retryable(
       value = {RestClientException.class},
       maxAttemptsExpression = "#{${retries.maxAttempts}}",
       backoff = @Backoff(delayExpression = "#{${retries.backoff}}"))
   public SurveyClassifierTypeDTO requestClassifierTypeSelector(
-      final UUID surveyId, final UUID classifierType) throws RestClientException {
-
+      final UUID surveyId, final UUID classifierType) {
+    log.with("survey_id", surveyId.toString())
+        .with("classifier_type", classifierType.toString())
+        .debug("Requesting survey classifier type");
     UriComponents uriComponents =
         restUtility.createUriComponents(
             appConfig.getSurveySvc().getRequestClassifierTypesPath(),
             null,
             surveyId,
             classifierType);
-
-    HttpEntity<?> httpEntity = restUtility.createHttpEntity(null);
-
-    log.with("survey_id", surveyId.toString())
-        .with("classifier_type", classifierType.toString())
-        .debug("Requesting survey classifier type");
-    ResponseEntity<String> responseEntity =
-        restTemplate.exchange(uriComponents.toUri(), HttpMethod.GET, httpEntity, String.class);
-
-    SurveyClassifierTypeDTO result = null;
-    if (responseEntity != null && responseEntity.getStatusCode().is2xxSuccessful()) {
-      String responseBody = responseEntity.getBody();
-      try {
-        result = objectMapper.readValue(responseBody, SurveyClassifierTypeDTO.class);
-      } catch (IOException e) {
-        String msg = String.format("cause = %s - message = %s", e.getCause(), e.getMessage());
-        log.error(msg);
-      }
-    }
-    return result;
+    HttpEntity<?> httpEntity = restUtility.createHttpEntityWithAuthHeader();
+    ResponseEntity<SurveyClassifierTypeDTO> responseEntity =
+        restTemplate.exchange(
+            uriComponents.toUri(), HttpMethod.GET, httpEntity, SurveyClassifierTypeDTO.class);
+    return responseEntity.getBody();
   }
 
-  private SurveyDTO getSurveyDtoFromResponseEntity(ResponseEntity<String> responseEntity) {
-    SurveyDTO result = null;
-    if (responseEntity != null && responseEntity.getStatusCode().is2xxSuccessful()) {
-      String responseBody = responseEntity.getBody();
-      try {
-        result = objectMapper.readValue(responseBody, SurveyDTO.class);
-      } catch (IOException e) {
-        String msg = String.format("cause = %s - message = %s", e.getCause(), e.getMessage());
-        log.error(msg);
-      }
-    }
-    return result;
-  }
-
-  /**
-   * Get classifier type selector for Survey UUID and ClassifierType UUID.
-   *
-   * @param surveyId UUID for which to request classifiers.
-   * @return SurveyDTO representation of survey
-   * @throws RestClientException something went wrong making http call.
-   */
-  @Retryable(
-      value = {RestClientException.class},
-      maxAttemptsExpression = "#{${retries.maxAttempts}}",
-      backoff = @Backoff(delayExpression = "#{${retries.backoff}}"))
   public SurveyDTO findSurvey(final UUID surveyId) {
     UriComponents uriComponents =
         restUtility.createUriComponents(
             appConfig.getSurveySvc().getSurveyDetailPath(), null, surveyId);
 
-    HttpEntity<?> httpEntity = restUtility.createHttpEntity(null);
-    ResponseEntity<String> responseEntity;
-    SurveyDTO survey;
+    HttpEntity<?> httpEntity = restUtility.createHttpEntityWithAuthHeader();
+    ResponseEntity<SurveyDTO> responseEntity;
     try {
       log.with("survey_id", surveyId.toString()).debug("Retrieving survey");
       responseEntity =
-          restTemplate.exchange(uriComponents.toUri(), HttpMethod.GET, httpEntity, String.class);
-      survey = getSurveyDtoFromResponseEntity(responseEntity);
+          restTemplate.exchange(uriComponents.toUri(), HttpMethod.GET, httpEntity, SurveyDTO.class);
     } catch (HttpClientErrorException e) {
       if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
         return null;
@@ -180,7 +119,7 @@ public class SurveySvcClient {
       log.error("Failed to retrieve survey", e);
       throw e;
     }
-    return survey;
+    return responseEntity.getBody();
   }
 
   @Retryable(
@@ -192,15 +131,11 @@ public class SurveySvcClient {
         restUtility.createUriComponents(
             appConfig.getSurveySvc().getSurveyRefPath(), null, surveyRef);
 
-    HttpEntity<?> httpEntity = restUtility.createHttpEntity(null);
-    ResponseEntity<String> responseEntity = null;
-    SurveyDTO survey = null;
-
+    HttpEntity<?> httpEntity = restUtility.createHttpEntityWithAuthHeader();
+    ResponseEntity<SurveyDTO> responseEntity;
     try {
       responseEntity =
-          restTemplate.exchange(uriComponents.toUri(), HttpMethod.GET, httpEntity, String.class);
-
-      survey = getSurveyDtoFromResponseEntity(responseEntity);
+          restTemplate.exchange(uriComponents.toUri(), HttpMethod.GET, httpEntity, SurveyDTO.class);
     } catch (HttpClientErrorException e) {
       if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
         return null;
@@ -208,7 +143,7 @@ public class SurveySvcClient {
       log.error("Failed to retrieve survey", e);
       throw e;
     }
-    return survey;
+    return responseEntity.getBody();
   }
 
   public SurveyDTO getSurveyForCollectionExercise(final CollectionExercise collex)
--- src/test/java/uk/gov/ons/ctp/response/collection/exercise/client/SurveySvcClientTest.java
+++ src/test/java/uk/gov/ons/ctp/response/collection/exercise/client/SurveySvcClientTest.java
@@ -1,8 +1,6 @@
 package uk.gov.ons.ctp.response.collection.exercise.client;
 
-import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
@@ -10,9 +8,8 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
 import java.net.URI;
+import java.util.List;
 import java.util.UUID;
 import org.junit.Before;
 import org.junit.Test;
@@ -21,126 +18,216 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
-import uk.gov.ons.ctp.common.error.CTPException;
 import uk.gov.ons.ctp.common.rest.RestUtility;
 import uk.gov.ons.ctp.common.rest.RestUtilityConfig;
 import uk.gov.ons.ctp.response.collection.exercise.config.AppConfig;
 import uk.gov.ons.ctp.response.collection.exercise.config.SurveySvc;
-import uk.gov.ons.ctp.response.collection.exercise.domain.CollectionExercise;
+import uk.gov.ons.response.survey.representation.SurveyClassifierDTO;
+import uk.gov.ons.response.survey.representation.SurveyClassifierTypeDTO;
 import uk.gov.ons.response.survey.representation.SurveyDTO;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SurveySvcClientTest {
 
-  private static final String SURVEY_PATH = "/surveys";
-  private static final String SURVEY_REF_PATH = SURVEY_PATH + "/ref";
+  private static final UUID SURVEY_ID = UUID.fromString("ede0c029-3109-4ae1-b8c8-e4968000e2f4");
+  private static final UUID CLASSIFIER_TYPE =
+      UUID.fromString("dee0c029-3109-4ae1-b8c8-e4968000e2e5");
+  private static final String SURVEY_REF = "ABC123";
+  private static final String SURVEY_REF_PATH = "/surveys/ref";
 
   @Mock private RestTemplate restTemplate;
 
   @Spy private final RestUtility restUtility = new RestUtility(RestUtilityConfig.builder().build());
 
-  @InjectMocks private SurveySvcClient surveySvcClient;
-
-  @Mock private ObjectMapper objectMapper;
-
   @Mock private AppConfig appConfig;
 
+  @InjectMocks private SurveySvcClient surveySvcClient;
+
   @Before
-  public void prepareSurveyService() {
+  public void setUp() {
     SurveySvc surveySvc = new SurveySvc();
     surveySvc.setSurveyRefPath(SURVEY_REF_PATH);
     given(appConfig.getSurveySvc()).willReturn(surveySvc);
   }
 
-  @Test(expected = RestClientException.class)
-  public void ensure4xxThrownSurveyFindByRef() {
-    String surveyRef = "ABC123";
+  @Test
+  public void findSurveyByRef_200Response() {
+    // Given
+    when(restTemplate.exchange(
+            any(URI.class), eq(HttpMethod.GET), any(HttpEntity.class), eq(SurveyDTO.class)))
+        .thenReturn(new ResponseEntity<>(HttpStatus.OK));
 
-    given(
-            restTemplate.exchange(
-                any(URI.class), eq(HttpMethod.GET), any(HttpEntity.class), eq(String.class)))
-        .willThrow(new RestClientException("Bad request"));
+    // When
+    surveySvcClient.findSurveyByRef(SURVEY_REF);
 
-    surveySvcClient.findSurveyByRef(surveyRef);
-
+    // Then
     verify(restTemplate, times(1))
-        .exchange(any(), eq(HttpMethod.GET), any(HttpEntity.class), eq(String.class));
+        .exchange(any(), eq(HttpMethod.GET), any(HttpEntity.class), eq(SurveyDTO.class));
   }
 
   @Test
-  public void ensureNullReturnedOnNullSurvey() {
-    UUID surveyId = UUID.randomUUID();
+  public void findSurveyByRef_404Response() {
+    // Given
+    when(restTemplate.exchange(
+            any(URI.class), eq(HttpMethod.GET), any(HttpEntity.class), eq(SurveyDTO.class)))
+        .thenThrow(new HttpClientErrorException(HttpStatus.NOT_FOUND));
 
-    given(
-            restTemplate.exchange(
-                any(URI.class), eq(HttpMethod.GET), any(HttpEntity.class), eq(String.class)))
-        .willThrow(new HttpClientErrorException(HttpStatus.NOT_FOUND, "Bad request"));
+    // When
+    SurveyDTO survey = surveySvcClient.findSurveyByRef(SURVEY_REF);
 
-    SurveyDTO survey = surveySvcClient.findSurvey(surveyId);
+    // Then
+    verify(restTemplate, times(1))
+        .exchange(any(), eq(HttpMethod.GET), any(HttpEntity.class), eq(SurveyDTO.class));
+    assertNull(survey);
+  }
 
-    // When a 404 error is thrown, the function will return a null survey
-    assertNull("Survey was not null as expected", survey);
+  @Test(expected = HttpClientErrorException.class)
+  public void findSurveyByRef_500Response() {
+    // Given
+    when(restTemplate.exchange(
+            any(URI.class), eq(HttpMethod.GET), any(HttpEntity.class), eq(SurveyDTO.class)))
+        .thenThrow(new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR));
+
+    // When
+    surveySvcClient.findSurveyByRef(SURVEY_REF);
+
+    // Then
+    verify(restTemplate, times(1))
+        .exchange(any(), eq(HttpMethod.GET), any(HttpEntity.class), eq(SurveyDTO.class));
   }
 
   @Test
-  public void ensureSurveyReturnedOnGetSurvey() throws IOException {
-    final UUID surveyId = UUID.randomUUID();
+  public void findSurvey_200Response() {
+    // Given
+    when(restTemplate.exchange(
+            any(URI.class), eq(HttpMethod.GET), any(HttpEntity.class), eq(SurveyDTO.class)))
+        .thenReturn(new ResponseEntity<>(HttpStatus.OK));
 
-    final ResponseEntity<String> responseEntity = new ResponseEntity<>("json", HttpStatus.OK);
-    given(
-            restTemplate.exchange(
-                any(URI.class), eq(HttpMethod.GET), any(HttpEntity.class), eq(String.class)))
-        .willReturn(responseEntity);
+    // When
+    surveySvcClient.findSurvey(SURVEY_ID);
 
-    final SurveyDTO surveyDto = new SurveyDTO();
-
-    when(objectMapper.readValue("json", SurveyDTO.class)).thenReturn(surveyDto);
-
-    final SurveyDTO returnedSurvey = surveySvcClient.findSurvey(surveyId);
-
-    assertThat(returnedSurvey, is(surveyDto));
+    // Then
+    verify(restTemplate, times(1))
+        .exchange(any(), eq(HttpMethod.GET), any(HttpEntity.class), eq(SurveyDTO.class));
   }
 
   @Test
-  public void ensureGetSurveyForCollectionExerciseReturnsSurvey() throws IOException, CTPException {
-    final UUID surveyId = UUID.randomUUID();
-    final SurveyDTO surveyDto = new SurveyDTO();
-    surveyDto.setId(surveyId.toString());
+  public void findSurvey_404Response() {
+    // Given
+    when(restTemplate.exchange(
+            any(URI.class), eq(HttpMethod.GET), any(HttpEntity.class), eq(SurveyDTO.class)))
+        .thenThrow(new HttpClientErrorException(HttpStatus.NOT_FOUND));
 
-    final CollectionExercise collex = new CollectionExercise();
-    collex.setSurveyId(surveyId);
+    // When
+    SurveyDTO survey = surveySvcClient.findSurvey(SURVEY_ID);
 
-    final ResponseEntity<String> responseEntity = new ResponseEntity<>("json", HttpStatus.OK);
-    given(
-            restTemplate.exchange(
-                any(URI.class), eq(HttpMethod.GET), any(HttpEntity.class), eq(String.class)))
-        .willReturn(responseEntity);
-
-    when(objectMapper.readValue("json", SurveyDTO.class)).thenReturn(surveyDto);
-
-    final SurveyDTO returnedSurvey = surveySvcClient.getSurveyForCollectionExercise(collex);
-
-    assertThat(returnedSurvey, is(surveyDto));
+    // Then
+    verify(restTemplate, times(1))
+        .exchange(any(), eq(HttpMethod.GET), any(HttpEntity.class), eq(SurveyDTO.class));
+    assertNull(survey);
   }
 
-  @Test(expected = CTPException.class)
-  public void throwCTPExceptionIfSurveyNotFoundForCollectionExercise() throws CTPException {
-    final UUID surveyId = UUID.randomUUID();
-    final CollectionExercise collectionExercse = new CollectionExercise();
-    collectionExercse.setSurveyId(surveyId);
+  @Test(expected = HttpClientErrorException.class)
+  public void findSurvey_500Response() {
+    // Given
+    when(restTemplate.exchange(
+            any(URI.class), eq(HttpMethod.GET), any(HttpEntity.class), eq(SurveyDTO.class)))
+        .thenThrow(new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR));
 
-    given(
-            restTemplate.exchange(
-                any(URI.class), eq(HttpMethod.GET), any(HttpEntity.class), eq(String.class)))
-        .willThrow(new HttpClientErrorException(HttpStatus.NOT_FOUND, "Bad request"));
+    // When
+    surveySvcClient.findSurvey(SURVEY_ID);
 
-    surveySvcClient.getSurveyForCollectionExercise(collectionExercse);
+    // Then
+    verify(restTemplate, times(1))
+        .exchange(any(), eq(HttpMethod.GET), any(HttpEntity.class), eq(SurveyDTO.class));
+  }
+
+  @Test
+  public void requestClassifierTypeSelector_200Response() {
+    // Given
+    when(restTemplate.exchange(
+            any(URI.class),
+            eq(HttpMethod.GET),
+            any(HttpEntity.class),
+            eq(SurveyClassifierTypeDTO.class)))
+        .thenReturn(new ResponseEntity<>(HttpStatus.OK));
+
+    // When
+    surveySvcClient.requestClassifierTypeSelector(SURVEY_ID, CLASSIFIER_TYPE);
+
+    // Then
+    verify(restTemplate, times(1))
+        .exchange(
+            any(), eq(HttpMethod.GET), any(HttpEntity.class), eq(SurveyClassifierTypeDTO.class));
+  }
+
+  @Test(expected = HttpClientErrorException.class)
+  public void requestClassifierTypeSelector_500Response() {
+    // Given
+    when(restTemplate.exchange(
+            any(URI.class),
+            eq(HttpMethod.GET),
+            any(HttpEntity.class),
+            eq(SurveyClassifierTypeDTO.class)))
+        .thenThrow(new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR));
+
+    // When
+    surveySvcClient.requestClassifierTypeSelector(SURVEY_ID, CLASSIFIER_TYPE);
+
+    // Then
+    verify(restTemplate, times(1))
+        .exchange(
+            any(), eq(HttpMethod.GET), any(HttpEntity.class), eq(SurveyClassifierTypeDTO.class));
+  }
+
+  @Test
+  public void requestClassifierTypeSelectors_200Response() {
+    // Given
+    when(restTemplate.exchange(
+            any(URI.class),
+            eq(HttpMethod.GET),
+            any(HttpEntity.class),
+            eq(new ParameterizedTypeReference<List<SurveyClassifierDTO>>() {})))
+        .thenReturn(new ResponseEntity<>(HttpStatus.OK));
+
+    // When
+    surveySvcClient.requestClassifierTypeSelectors(SURVEY_ID);
+
+    // Then
+    verify(restTemplate, times(1))
+        .exchange(
+            any(),
+            eq(HttpMethod.GET),
+            any(HttpEntity.class),
+            eq(new ParameterizedTypeReference<List<SurveyClassifierDTO>>() {}));
+  }
+
+  @Test(expected = HttpClientErrorException.class)
+  public void requestClassifierTypeSelectors_500Response() {
+    // Given
+    when(restTemplate.exchange(
+            any(URI.class),
+            eq(HttpMethod.GET),
+            any(HttpEntity.class),
+            eq(new ParameterizedTypeReference<List<SurveyClassifierDTO>>() {})))
+        .thenThrow(new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR));
+
+    // When
+    surveySvcClient.requestClassifierTypeSelectors(SURVEY_ID);
+
+    // Then
+    verify(restTemplate, times(1))
+        .exchange(
+            any(),
+            eq(HttpMethod.GET),
+            any(HttpEntity.class),
+            eq(new ParameterizedTypeReference<List<SurveyClassifierDTO>>() {}));
   }
 }
