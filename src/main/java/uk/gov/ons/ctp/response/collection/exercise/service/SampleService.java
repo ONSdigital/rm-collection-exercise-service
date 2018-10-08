e28f48a072635426a871268e328816df0e7ef5fd
Nick Grant <nick.grant@ext.ons.gov.uk>
2018-10-08 11:09:48 +0100

    Remove unused import

--- src/main/java/uk/gov/ons/ctp/response/collection/exercise/service/SampleService.java
+++ src/main/java/uk/gov/ons/ctp/response/collection/exercise/service/SampleService.java
@@ -11,7 +11,6 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.ons.ctp.common.error.CTPException;
 import uk.gov.ons.ctp.response.collection.exercise.client.PartySvcClient;
