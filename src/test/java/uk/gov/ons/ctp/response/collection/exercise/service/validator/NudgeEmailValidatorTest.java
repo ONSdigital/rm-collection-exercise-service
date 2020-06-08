f71fbbc694f1c3fc872e2507ea3fd96f71b23560
Amit Sinha <aks.sinha.amit@icloud.com>
2020-06-08 13:15:45 +0100

    Fix for event update for nudge email fails for the same event with the validation error

--- src/test/java/uk/gov/ons/ctp/response/collection/exercise/service/validator/NudgeEmailValidatorTest.java
+++ src/test/java/uk/gov/ons/ctp/response/collection/exercise/service/validator/NudgeEmailValidatorTest.java
@@ -233,4 +233,36 @@ public class NudgeEmailValidatorTest {
         "A nudge email has already been scheduled for this date and time. Choose a different date or time.",
         actualException.getMessage());
   }
+
+  @Test
+  public void testNudgeWithSameDateAndTimeEventCreationNotValidForTheSameEvent() {
+    final Instant now = Instant.now();
+    final Long nudgeTime = now.plus(2, ChronoUnit.DAYS).toEpochMilli();
+    final Event goLive = new Event();
+    goLive.setTag((EventService.Tag.go_live.toString()));
+    goLive.setTimestamp(Timestamp.from(now));
+
+    final Event nudge = new Event();
+    nudge.setTag((EventService.Tag.nudge_email_0.toString()));
+    nudge.setTimestamp(new Timestamp(nudgeTime));
+
+    final Event returnBy = new Event();
+    returnBy.setTag((EventService.Tag.return_by.toString()));
+    returnBy.setTimestamp(Timestamp.from(now.plus(4, ChronoUnit.DAYS)));
+
+    final List<Event> events = Arrays.asList(goLive, returnBy, nudge);
+
+    final Event submittedEvent = new Event();
+    submittedEvent.setTag((EventService.Tag.nudge_email_0.toString()));
+    submittedEvent.setTimestamp(new Timestamp(nudgeTime));
+
+    CTPException actualException = null;
+    try {
+      nudgeEmailValidator.validate(
+          events, submittedEvent, CollectionExerciseDTO.CollectionExerciseState.CREATED);
+    } catch (CTPException expectedException) {
+      actualException = expectedException;
+    }
+    assertNull(actualException);
+  }
 }
