cc0b25d999749633768f0e7b87a01c41e83ae9c2
Adam Wilkie <Adam.Wilkie@ons.gov.uk>
2020-11-19 11:50:02 +0000

    created database migration for deleting tables and procedures

---
+++ src/main/resources/database/changes/release-12/remove_report_table_and_procedures.sql
@@ -0,0 +1,7 @@
+DROP TABLE [IF EXISTS]
+    collectionexercise.messagelog,
+    collectionexercise.report,
+    collectionexercise.reporttype;
+DROP FUNCTION [IF EXISTS]
+    collectionexercise.generate_collectionexercise_mi,
+    collectionexercise.logmessage;
