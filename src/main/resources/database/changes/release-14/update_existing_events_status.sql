246ea166258dd86dff9ba1e7d8cfef87aee699ee
jdfox <jfoxgithub@gmail.com>
2020-12-08 17:24:33 +0000

    simplify query

--- src/main/resources/database/changes/release-14/update_existing_events_status.sql
+++ src/main/resources/database/changes/release-14/update_existing_events_status.sql
@@ -1,15 +1,11 @@
-UPDATE collectionexercise.collectionexercise AS c
-SET statefk = 'SCHEDULED'
-FROM collectionexercise.event AS e
-WHERE c.exercisepk = e.collexfk
-  AND c.statefk = 'NOT_SET'
+UPDATE collectionexercise.event AS e
+SET status = 'SCHEDULED'
+WHERE e.status = 'NOT_SET'
   AND e.timestamp >= now()
   AND e.timestamp IS NOT NULL;
 
-UPDATE collectionexercise.collectionexercise AS c
-SET statefk = 'PROCESSED'
-FROM collectionexercise.event AS e
-WHERE c.exercisepk = e.collexfk
-  AND c.statefk = 'NOT_SET'
+UPDATE collectionexercise.event AS e
+SET status = 'PROCESSED'
+WHERE e.status = 'NOT_SET'
   AND e.timestamp < now()
   AND e.timestamp IS NOT NULL;
