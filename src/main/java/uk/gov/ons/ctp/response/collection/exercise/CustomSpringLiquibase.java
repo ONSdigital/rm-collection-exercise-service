8dce332ed3d8e27d7b67ef68ae1c39ee16c57976
Adam Mann <manny160389@hotmail.com>
2022-04-25 11:41:47 +0100

    Downgrade to 2.5.12 to get the tests passing

--- src/main/java/uk/gov/ons/ctp/response/collection/exercise/CustomSpringLiquibase.java
+++ src/main/java/uk/gov/ons/ctp/response/collection/exercise/CustomSpringLiquibase.java
@@ -6,6 +6,15 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ResourceLoaderAware;
 import org.springframework.core.io.ResourceLoader;
 
+/**
+ * This class was necessary due to the changes made in Spring Boot 2.55, which changed the order of
+ * SQL loading. It means we now need to define our own Liquibase integration and set it to run after
+ * Hibernate does.
+ *
+ * <p>This leads to an issue in dev environments where, on first run, it'll fail to make the iac
+ * table This can be safely ignored and it'll run properly on subsequent runs This isn't an issue in
+ * preprod or prod as the schemas already exist
+ */
 public class CustomSpringLiquibase implements InitializingBean, BeanNameAware, ResourceLoaderAware {
   private SpringLiquibase springLiquibase;
 
