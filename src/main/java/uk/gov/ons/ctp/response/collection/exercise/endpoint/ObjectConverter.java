d01606668ab6d72158cb929e6c7962afd2d8bb0a
SteveScorfield <steve.scorfield@ons.gov.uk>
2022-06-20 15:19:25 +0100

    Initial commit of changes

---
+++ src/main/java/uk/gov/ons/ctp/response/collection/exercise/endpoint/ObjectConverter.java
@@ -0,0 +1,55 @@
+package uk.gov.ons.ctp.response.collection.exercise.endpoint;
+
+import java.util.ArrayList;
+import java.util.List;
+import uk.gov.ons.ctp.response.collection.exercise.domain.CollectionExercise;
+import uk.gov.ons.ctp.response.collection.exercise.domain.SampleLink;
+import uk.gov.ons.ctp.response.collection.exercise.representation.CollectionExerciseDTO;
+import uk.gov.ons.ctp.response.collection.exercise.representation.SampleLinkDTO;
+
+public class ObjectConverter {
+
+  private ObjectConverter() {};
+
+  public static List<SampleLinkDTO> sampleLinkDTO(List<SampleLink> sampleLinks) {
+    List<SampleLinkDTO> mappedSampleLink = new ArrayList<>();
+    for (int i = 0; i <= sampleLinks.size(); i++) {
+      SampleLink oldSampleLink = sampleLinks.get(i);
+      SampleLinkDTO sampleLinkDTO = new SampleLinkDTO();
+
+      sampleLinkDTO.setSampleSummaryId(oldSampleLink.getSampleSummaryId().toString());
+      sampleLinkDTO.setCollectionExerciseId(oldSampleLink.getCollectionExerciseId().toString());
+
+      mappedSampleLink.add(sampleLinkDTO);
+    }
+    return mappedSampleLink;
+  }
+
+  public static CollectionExerciseDTO collectionExerciseDTO(CollectionExercise collectionExercise) {
+    CollectionExerciseDTO collectionExerciseDTO = new CollectionExerciseDTO();
+    collectionExerciseDTO.setExerciseRef(collectionExerciseDTO.getExerciseRef());
+    collectionExerciseDTO.setSurveyRef(collectionExerciseDTO.getSurveyRef());
+    collectionExerciseDTO.setCreated(collectionExercise.getCreated());
+    collectionExerciseDTO.setEqVersion(collectionExerciseDTO.getEqVersion());
+    collectionExerciseDTO.setDeleted(collectionExercise.getDeleted());
+    collectionExerciseDTO.setId(collectionExercise.getId());
+    collectionExerciseDTO.setActualExecutionDateTime(
+        collectionExercise.getActualExecutionDateTime());
+    collectionExerciseDTO.setActualPublishDateTime(collectionExercise.getActualPublishDateTime());
+    collectionExerciseDTO.setExecutedBy(collectionExercise.getExecutedBy());
+    collectionExerciseDTO.setPeriodEndDateTime(collectionExercise.getPeriodEndDateTime());
+    collectionExerciseDTO.setPeriodStartDateTime(collectionExercise.getPeriodStartDateTime());
+    collectionExerciseDTO.setScheduledEndDateTime(collectionExercise.getScheduledEndDateTime());
+    collectionExerciseDTO.setScheduledExecutionDateTime(
+        collectionExercise.getScheduledExecutionDateTime());
+    collectionExerciseDTO.setScheduledReturnDateTime(
+        collectionExercise.getScheduledReturnDateTime());
+    collectionExerciseDTO.setScheduledStartDateTime(collectionExercise.getScheduledStartDateTime());
+    collectionExerciseDTO.setState(collectionExercise.getState());
+    collectionExerciseDTO.setSurveyId(collectionExercise.getSurveyId().toString());
+    collectionExerciseDTO.setUpdated(collectionExercise.getUpdated());
+    collectionExerciseDTO.setUserDescription(collectionExercise.getUserDescription());
+
+    return collectionExerciseDTO;
+  }
+}
