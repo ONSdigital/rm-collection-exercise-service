b652e005ffd2bf20f131f8a7ec39d2284076e555
“JacobHarrisonONS” <jacobharrison@outlook.com>
2023-02-06 11:10:58 +0000

    temp comment out IT tests to see if I can get actual redis to work correctly

--- src/main/java/uk/gov/ons/ctp/response/collection/exercise/config/RedisConfig.java
+++ src/main/java/uk/gov/ons/ctp/response/collection/exercise/config/RedisConfig.java
@@ -3,26 +3,37 @@ package uk.gov.ons.ctp.response.collection.exercise.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
 
   @Bean
-  public LettuceConnectionFactory redisConnectionFactory() {
-    //    RedisStandaloneConfiguration redisStandaloneConfiguration =
-    //        new RedisStandaloneConfiguration("localhost", 6379);
-    //    // this is 3 in dev will need to confirm in other envs
-    //    redisStandaloneConfiguration.setDatabase(3);
-    //    return new LettuceConnectionFactory(redisStandaloneConfiguration);
-    return new LettuceConnectionFactory();
+  protected JedisConnectionFactory jedisConnectionFactory() {
+    RedisStandaloneConfiguration configuration =
+        new RedisStandaloneConfiguration("localhost", 6379);
+    configuration.setDatabase(3);
+    JedisClientConfiguration jedisClientConfiguration =
+        JedisClientConfiguration.builder().usePooling().build();
+    JedisConnectionFactory factory =
+        new JedisConnectionFactory(configuration, jedisClientConfiguration);
+    factory.afterPropertiesSet();
+    return factory;
   }
 
   @Bean
-  RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-    RedisTemplate<String, String> template = new RedisTemplate<>();
-    template.setConnectionFactory(redisConnectionFactory);
-    return template;
+  public RedisTemplate<String, Object> redisTemplate(
+      JedisConnectionFactory jedisConnectionFactory) {
+    final RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(jedisConnectionFactory);
+    return redisTemplate;
+  }
+
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    return jedisConnectionFactory();
   }
 }
