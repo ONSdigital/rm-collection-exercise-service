814238629e0c4369d645d771cd9a7960a6eb14ca
Mark Price <mark.price@ons.gov.uk>
2023-07-17 16:11:05 +0100

    Temporarily add an example of the message from SDS

--- src/main/java/uk/gov/ons/ctp/response/collection/exercise/message/dto/SupplementaryDataServiceDTO.java
+++ src/main/java/uk/gov/ons/ctp/response/collection/exercise/message/dto/SupplementaryDataServiceDTO.java
@@ -13,6 +13,23 @@ import lombok.NoArgsConstructor;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SupplementaryDataServiceDTO {
 
+  /*  {
+      "survey_id": "012",
+      "period_id": "2013",
+      "form_types": [
+         "0017",
+         "0123",
+         "0001"
+        ],
+      "title": "A dataset version 4 for survey 2012 period 2013",
+      "sds_published_at": "2023-07-17T14:46:36Z",
+      "total_reporting_units": 2,
+      "schema_version": "v1.0.0",
+      "sds_dataset_version": 4,
+      "filename": "373d9a77-2ee5-4c1f-a6dd-8d07b0ea9793.json",
+      "dataset_id": "b9a87999-fcc0-4085-979f-06390fb5dddd"
+  }*/
+
   String datasetId;
   String surveyId;
   String periodId;
