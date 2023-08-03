89e0082749b2e2ccb11aa93b3fc228818dcc20dd
“JacobHarrisonONS” <jacobharrison@outlook.com>
2023-08-03 13:44:35 +0100

    remove sequence

--- src/main/resources/database/changes/release-20/supplementary_dataset_table.sql
+++ src/main/resources/database/changes/release-20/supplementary_dataset_table.sql
@@ -1,14 +1,5 @@
 Set schema 'collectionexercise';
 
--- Sequence: supplementarydatasetidseq
--- DROP SEQUENCE supplementarydatasetidseq;
--- CREATE SEQUENCE IF NOT EXISTS supplementarydatasetidseq
---   INCREMENT 1
---   MINVALUE 1
---   MAXVALUE 999999999999
---   START 1
---   CACHE 1;
-
 -- Table: supplementarydataset
 CREATE TABLE supplementarydataset
 ( id         int NOT NULL,
