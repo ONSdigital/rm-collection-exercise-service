14d9d4a500e7acf7700520b7064a8a1a21148e18
SteveScorfield <steve.scorfield@ons.gov.uk>
2023-08-07 08:44:13 +0100

    Have updated the delete query

--- src/main/java/uk/gov/ons/ctp/response/collection/exercise/repository/SupplementaryDatasetRepository.java
+++ src/main/java/uk/gov/ons/ctp/response/collection/exercise/repository/SupplementaryDatasetRepository.java
@@ -1,6 +1,9 @@
 package uk.gov.ons.ctp.response.collection.exercise.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import uk.gov.ons.ctp.response.collection.exercise.domain.SupplementaryDatasetEntity;
 
 /** JPA Data Repository. */
@@ -9,5 +12,7 @@ public interface SupplementaryDatasetRepository
 
   boolean existsByExerciseFK(int exerciseFk);
 
-  void deleteByExerciseFK(int exerciseFk);
+  @Modifying
+  @Query("DELETE from SupplementaryDatasetEntity where exerciseFK = :sampleSummaryPK")
+  void deleteByExerciseFK(@Param("sampleSummaryPK") Integer exerciseFk);
 }
