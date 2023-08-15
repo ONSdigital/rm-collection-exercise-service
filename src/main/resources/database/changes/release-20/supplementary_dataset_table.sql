9fcc8ba0e370a79939c7f459cc795a521ea9b573
“JacobHarrisonONS” <jacobharrison@outlook.com>
2023-08-15 06:48:47 +0100

    clean up uniques in SDS table creation

--- src/main/resources/database/changes/release-20/supplementary_dataset_table.sql
+++ src/main/resources/database/changes/release-20/supplementary_dataset_table.sql
@@ -2,20 +2,11 @@ Set schema 'collectionexercise';
 
 -- Table: supplementarydataset
 CREATE TABLE supplementarydataset
-( id         int NOT NULL,
-  exercise_FK int NOT NULL,
-  supplementary_dataset_id  uuid NOT NULL,
+( id         int NOT NULL UNIQUE,
+  exercise_FK int NOT NULL UNIQUE,
+  supplementary_dataset_id  uuid NOT NULL UNIQUE,
   attributes character varying(10000) NOT NULL
 );
 
 -- -- Add foreign key
--- ALTER TABLE ONLY supplementarydataset ADD CONSTRAINT exercise_FK_fkey FOREIGN KEY (exercise_FK) REFERENCES collectionexercise(exercise_PK);
-
--- Add index
-ALTER TABLE supplementarydataset ADD CONSTRAINT supplementarydataset_id_uuid_key UNIQUE (id);
-
--- unique supplementary_dataset_id
-ALTER TABLE supplementarydataset ADD CONSTRAINT supplementary_dataset_id_unique UNIQUE (supplementary_dataset_id);
-
--- unique exercise_FK
-ALTER TABLE supplementarydataset ADD CONSTRAINT exercise_FK_unique UNIQUE (exercise_FK);
+ALTER TABLE supplementarydataset ADD CONSTRAINT exercise_FK_fkey FOREIGN KEY (exercise_FK) REFERENCES collectionexercise(exercise_PK);
