d8e8c409e3bd1ccd077ee053f76c1de3fae0b396
Mark Price <mark.price@ons.gov.uk>
2025-10-01 11:40:41 +0100

    Fixes

--- src/test/java/uk/gov/ons/ctp/response/collection/exercise/endpoint/CollectionExerciseExecutionEndpointUnitTests.java
+++ src/test/java/uk/gov/ons/ctp/response/collection/exercise/endpoint/CollectionExerciseExecutionEndpointUnitTests.java
@@ -1,26 +1,34 @@
 package uk.gov.ons.ctp.response.collection.exercise.endpoint;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static uk.gov.ons.ctp.lib.common.MvcHelper.*;
 import static uk.gov.ons.ctp.lib.common.utility.MockMvcControllerAdviceHelper.mockAdviceFor;
+import static uk.gov.ons.ctp.response.collection.exercise.lib.common.MvcHelper.postJson;
 
 import com.godaddy.logging.Logger;
 import com.godaddy.logging.LoggerFactory;
 import java.util.List;
 import java.util.UUID;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import uk.gov.ons.ctp.lib.common.FixtureHelper;
 import uk.gov.ons.ctp.response.collection.exercise.lib.common.error.RestExceptionHandler;
 import uk.gov.ons.ctp.response.collection.exercise.lib.common.jackson.CustomObjectMapper;
 import uk.gov.ons.ctp.response.collection.exercise.lib.sample.representation.SampleUnitsRequestDTO;
+import uk.gov.ons.ctp.response.collection.exercise.service.SampleSummaryService;
 
 /** Collection Exercise Endpoint Unit tests */
 public class CollectionExerciseExecutionEndpointUnitTests {
+
+  @Mock private SampleSummaryService sampleSummaryService;
+
   private static final Logger log =
       LoggerFactory.getLogger(CollectionExerciseExecutionEndpointUnitTests.class);
 
@@ -57,20 +65,22 @@ public class CollectionExerciseExecutionEndpointUnitTests {
    *
    * @throws Exception exception thrown
    */
-  //  @Test
-  //  public void requestSampleUnits() throws Exception {
-  //
-  //    ResultActions actions =
-  //        mockCollectionExerciseExecutionMvc.perform(
-  //            postJson(
-  //                String.format("/collectionexerciseexecution/%s", COLLECTIONEXERCISE_ID1),
-  // "{}"));
-  //
-  //    actions
-  //        .andExpect(status().isOk())
-  //        .andExpect(handler().handlerType(CollectionExerciseExecutionEndpoint.class))
-  //        .andExpect(handler().methodName("requestSampleUnits"))
-  //        .andExpect(jsonPath("$.*", hasSize(1)))
-  //        .andExpect(jsonPath("$.sampleUnitsTotal", is(SAMPLEUNITSTOTAL)));
-  //  }
+    @Test
+    public void requestSampleUnits() throws Exception {
+
+      ResultActions actions =
+          mockCollectionExerciseExecutionMvc.perform(
+              postJson(
+                  String.format("/collectionexerciseexecution/%s", COLLECTIONEXERCISE_ID1),
+   "{}"));
+
+      String responseJson = actions.andReturn().getResponse().getContentAsString();
+
+      actions
+          .andExpect(status().isOk())
+          .andExpect(handler().handlerType(CollectionExerciseExecutionEndpoint.class))
+          .andExpect(handler().methodName("setReadyForLive"))
+          .andExpect(jsonPath("$.*", hasSize(1)))
+          .andExpect(jsonPath("$.sampleUnitsTotal", is(SAMPLEUNITSTOTAL)));
+    }
 }
